0c30578e3682677225fc28b7aaa54c4312e55968
rajkumarakula <bhavaniomega1998@gmail.com>
2024-04-03 17:00:20 +0530

    pdf created succesffulll

--- src/main/java/dev/raj/freightfox/Services/pdfserviceImple.java
+++ src/main/java/dev/raj/freightfox/Services/pdfserviceImple.java
@@ -14,19 +14,7 @@ public class pdfserviceImple implements pdfService{
 
     @Override
     public Context createPdf(pdftype pdfinfo) {
-//        pdftype pdftype1 = new pdftype();
-//        pdftype1.setItems(pdfinfo.getItems());
-//        pdftype1.setBuyer(pdfinfo.getBuyer());
-//        pdftype1.setSeller(pdfinfo.getSeller());
-//        pdftype1.setBuyerAddress(pdfinfo.getBuyerAddress());
-//        pdftype1.setSellerAddress(pdfinfo.getSellerAddress());
-//        pdftype1.setBuyerGstin(pdfinfo.getBuyerGstin());
-//        pdftype1.setSellerGstin(pdfinfo.getSellerGstin());
-////         Item item = new Item();
-////     for(Item item1 : pdfinfo.getItems()){
-////         pdftype1.setItems(); item1.getName();
-////     };
-        //pdftype1.setItems(pdfinfo.getItems());
+
         Context context = new Context();
         Map<String,Object> map = new HashMap<>();
         map.put("users",pdfinfo);
